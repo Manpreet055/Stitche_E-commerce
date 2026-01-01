0580c4aba851a12ebb654fcd757dca9da15b9d7a
Manpreet055 <59128809+Manpreet055@users.noreply.github.com>
2026-01-01 16:43:48 +0400

    validation: contact message form

--- src/layout/contact/MessageForm.jsx
+++ src/layout/contact/MessageForm.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import ContactDetails from "./ContactDetails";
 import { useUser } from "../../context/UserDataProvider";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { motion } from "framer-motion";
-import { Check, SendHorizonal } from "lucide-react";
+import { Check, CircleAlert, SendHorizonal } from "lucide-react";
 
 const MessageForm = () => {
   const api = useAxiosPrivate();
-  const { user, setError } = useUser();
+  const [loadingState, setLoadingState] = useState(false);
+  const [error, setError] = useState("");
+  const { user } = useUser();
   const {
     handleSubmit,
     reset,
     register,
-    formState: { isSubmitSuccessful },
+    formState: { isSubmitSuccessful, errors },
   } = useForm({
     defaultValues: {
       name: "",
@@ -25,10 +27,16 @@ const MessageForm = () => {
   const handleForm = async (data) => {
     data.user = user._id;
     try {
+      setLoadingState(true);
       await api.post("/inbox", data);
       reset();
     } catch (error) {
       setError(error.message);
+    } finally {
+      setTimeout(() => {
+        setLoadingState(false);
+        setError("");
+      }, 1000);
     }
   };
   return (
@@ -40,7 +48,7 @@ const MessageForm = () => {
     >
       <form
         onSubmit={handleSubmit(handleForm)}
-        className="flex h-fit p-6 flex-col w-fit justify-center items-center  rounded theme text-theme"
+        className="flex h-fit p-6 flex-col w-fit justify-center items-center max-w-2xl  rounded theme text-theme"
       >
         <h2 className="font-bold w-full text-start text-4xl lg:text-6xl">
           Get in Touch
@@ -51,23 +59,39 @@ const MessageForm = () => {
             id="name"
             type="text"
             {...register("name", {
-              required: true,
+              required: "Name is required",
+              minLength: 8,
+              maxLength: 25,
             })}
             className="p-2 py-4 w-full  sm:p-4 rounded-md sm:text-lg border-theme"
             placeholder="Name"
           />
+          {errors.name && (
+            <span className="text-red-500 text-xs px-2">
+              {errors.name.message}
+            </span>
+          )}
           <input
             type="text"
             {...register("email", {
-              required: true,
+              required: "Email is required",
+              minLength: 8,
+              maxLength: 50,
             })}
             className="p-2 py-4 w-full sm:p-4 rounded-md sm:text-lg border-theme"
             placeholder="Email"
           />
+          {errors.email && (
+            <span className="text-red-500 text-xs px-2">
+              {errors.email.message}
+            </span>
+          )}
         </div>
         <select
-          {...register("subject")}
-          className=" border-theme mb-3 p-2 py-4 rounded-md w-full sm:p-4"
+          {...register("subject", {
+            required: "Please choose a subject",
+          })}
+          className=" border-theme p-2 py-4 rounded-md w-full sm:p-4"
           name="subject"
           id="subject"
         >
@@ -79,28 +103,45 @@ const MessageForm = () => {
           <option value="Feedback">Feedback</option>
           <option value="Other">Other</option>
         </select>
+        {errors.subject && (
+          <span className="w-full flex mt-1 justify-start text-red-500 text-xs mb-1 px-2">
+            {errors.subject.message}
+          </span>
+        )}
 
         {/* Message */}
         <textarea
           {...register("message", {
-            required: true,
+            required: "Please type a Message",
+            minLength: 8,
+            maxLength: 150,
           })}
           name="message"
-          className="p-4 w-full rounded-md mb-4 sm:text-lg border-theme lg:min-h-46"
+          className="p-4 w-full rounded-md mt-4  sm:text-lg border-theme lg:min-h-46"
           placeholder="Message"
           id="msg"
         />
+        {errors.message && (
+          <span className="text-red-500 w-full flex justify-start text-xs mt-1 px-2">
+            {errors.message.message}
+          </span>
+        )}
         <button
-          disabled={isSubmitSuccessful}
-          className="rounded-md w-full disabled:bg-gray-400 py-3 flex justify-center hover:opacity-90 px-4 ease-in-out transition-all duration-300 theme-alt text-theme-alt"
+          disabled={loadingState}
+          className={`rounded-md w-full mt-4 ${loadingState ? "dark:bg-neutral-400" : "theme-alt text-theme-alt"}   disabled:bg-gray-400 py-3 flex justify-center hover:opacity-90 px-4 ease-in-out transition-all duration-300 `}
         >
-          {isSubmitSuccessful ? (
-            <span className="flex place-items-center gap-2 text-center">
+          {loadingState ? (
+            <span className="flex items-center gap-2 text-center">
               Message sent
               <Check size={18} />
             </span>
+          ) : error ? (
+            <span className="flex text-red-500 items-center gap-2 text-center">
+              {error}
+              <CircleAlert size={18} />
+            </span>
           ) : (
-            <span className="flex place-items-center gap-2 text-center">
+            <span className="flex  items-center gap-2 text-center">
               Send Message
               <SendHorizonal size={18} />
             </span>
